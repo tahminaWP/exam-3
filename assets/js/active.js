a5fc311facd8be44755b31d5a56d7927aefbf929
tahminaWP <tahminatir703@gmail.com>
2021-12-16 21:31:16 +0600

    first commit

---
+++ assets/js/active.js
@@ -0,0 +1,35 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		// code
+		$(".menu-bar").click(function(){
+			$(".menu").toggleClass("active");
+		});
+
+		$('.slider-area').owlCarousel({
+			loop:true,
+			dots:true,
+			nav:false,
+			autoplay:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1000:{
+					items:1
+				}
+			}
+		});
+		
+	});
+	
+	
+	jQuery(window).on("load", function(){
+		// code
+	});
+	
+})(jQuery);
